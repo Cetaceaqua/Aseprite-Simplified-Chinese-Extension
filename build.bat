93d7d388d3583a0fac08e6f7d26851d4b7e3feaa
Cetaceaqua <46396590+Cetaceaqua@users.noreply.github.com>
2023-12-02 19:23:21 +0800

    更新 build.bat

--- build.bat
+++ build.bat
@@ -6,6 +6,8 @@ title Build Extension Pack
 
 rem Please install 7-zip first, and add 7-zip installation directory to system environment variable
 
+if exist Aseprite-Simplified-Chinese-Extension.aseprite-extension del /q Aseprite-Simplified-Chinese-Extension.aseprite-extension
+
 7z a Aseprite-Simplified-Chinese-Extension.zip zh_cn.ini package.json
 
 ren Aseprite-Simplified-Chinese-Extension.zip Aseprite-Simplified-Chinese-Extension.aseprite-extension
